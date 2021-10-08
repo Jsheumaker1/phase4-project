4e534bb7250a6cca66c30bb1db1d1df2549fc117
Jsheumaker1 <Jsheumaker1@gmail.com>
2021-10-08 14:55:25 -0500

    jami

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -79,16 +79,7 @@ export default function Home({login, setUser}) {
                   </div>
                   <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                     <div className="flex-shrink-0 flex items-center">
-                      <img
-                        className="block lg:hidden h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        alt="Workflow"
-                      />
-                      <img
-                        className="hidden lg:block h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                        alt="Workflow"
-                      />
+                      <div className="mx-auto h-10 text-4xl w-auto" >💡</div>
                     </div>
                     <div className="hidden sm:block sm:ml-6">
                       <div className="flex space-x-4">
